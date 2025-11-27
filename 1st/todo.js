14a794b5e8ae3590223e719de0eb745ba9424cd2
world <hello@email.com>
2025-11-28 08:55:37 +0900

    2일차완료

--- 1st/todo.js
+++ 1st/todo.js
@@ -8,10 +8,17 @@ fs.readFile("./data.txt", "utf-8", (err, data) => {
     console.log(err);
     return;
   }
-  console.log(data.split("\r\n"));
   let students; // 남학생들만 모아서 출력하기.
+  students = data.split("\r\n").reduce((acc, elem) => {
+    let [id, name, gender, score] = elem.split(","); // array destructuring.
+    if (gender == "남") {
+      acc.push({ id, name, gender, score });
+    }
+    return acc;
+  }, []);
   // [{id: 1, name:'홍길동', gender:'남', score: 60},
   //  {id: 3, name:'이준호', gender:'남', score: 74}]
+  console.log(students);
 });
 
 console.log("end");
