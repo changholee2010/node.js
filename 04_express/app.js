ff534cffa163a177efc74df877f758930998684d
changho <guide@yedam.ac>
2026-02-23 17:54:43 +0900

    done

--- 04_express/app.js
+++ 04_express/app.js
@@ -1,7 +1,12 @@
 const express = require("express"); // 임포트.
 const fs = require("fs");
+const session = require("express-session");
+const fileStore = require("session-file-store")(session);
+const cors = require("cors");
+
 const app = express(); // 인스턴스.
 const customerRoute = require("./routes/customer");
+const compression = require("compression");
 
 // 정적파일 폴더(html,css,js)
 app.use(express.static("public"));
@@ -9,6 +14,25 @@ app.use(express.static("public"));
 // body parser 셋업.
 app.use(express.urlencoded()); // x-www-form-urlencoded
 app.use(express.json()); // json
+app.use("/customer/download", compression()); // 모든 라우팅에 적용.
+
+// session 관리.
+app.use(
+  session({
+    secret: "secret key",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      secure: false, // true=> https, false=> http
+      maxAge: 60000,
+    },
+    // store: new fileStore(), // 추가.
+  }),
+);
+
+// cors 셋업.
+app.use(cors());
 
 // 라우팅. 요청방식+URL(end point) => 실행할 함수.
 app.get("/", (req, res) => {
@@ -18,6 +42,29 @@ app.get("/", (req, res) => {
 // 외부 라우팅정보.
 app.use("/customer", customerRoute);
 app.use("/product", require("./routes/product"));
+
+app.get("/data", (req, res) => {
+  res.json({ id: "1001", data: "sample" });
+});
+
+// session
+app.get("/login", (req, res) => {
+  req.session.user = { id: "user99", name: "홍길동" };
+  console.log(req.session.user);
+  res.send("session에 저장.");
+});
+app.get("/logout", (req, res) => {
+  req.session.destroy(); // 세션삭제.
+  res.send("로그아웃.");
+});
+app.get("/my_info", (req, res) => {
+  console.log(req.session.user);
+  if (!req.session.user) {
+    res.json({ retCode: "NG", retMsg: "No user info" });
+    return;
+  }
+  res.json(req.session.user);
+});
 
 // express 에서 에러처리.
 app.use((err, req, res, next) => {
--- 04_express/routes/customer.js
+++ 04_express/routes/customer.js
@@ -21,10 +21,17 @@ router.post("/add", (req, res) => {
   res.send("Post방식 요청.");
 });
 
-// Get요청(parameter로처리.)
+// Get요청(parameter로처리.) => req.params 처리가능.
+// Post요청(body에 데이터 해석) => req.body 해석.
 router.post("/login", (req, res) => {
   console.log(req.body);
   res.send("login page.");
+});
+
+// compression() 미들웨어 적용. http://localhost:3000/customer/download
+router.get("/download", (req, res) => {
+  // res.send("compression() 모듈적용.");
+  res.download(path.join(__dirname, "..", "red.jpg"));
 });
 
 router.get("/error", (req, res) => {
